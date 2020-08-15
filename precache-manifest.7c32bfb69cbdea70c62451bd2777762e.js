78bb0df45b20ac9acad85135d90db232c4008143
Tim Chu <tchu@indeed.com>
2020-08-15 14:13:48 -0700

    Updates

--- precache-manifest.7c32bfb69cbdea70c62451bd2777762e.js
+++ precache-manifest.7c32bfb69cbdea70c62451bd2777762e.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5426d77a585330448a2aa882017f3e5a",
+    "revision": "1df3c0747c599b1a2dc0f6c7d3551b42",
     "url": "/index.html"
   },
   {
-    "revision": "bc0e02fd98a82d1c2c45",
-    "url": "/static/css/main.6f3a371c.chunk.css"
+    "revision": "4c4acf307b73ebea8d51",
+    "url": "/static/css/main.4812fc80.chunk.css"
   },
   {
     "revision": "2e7cceb88fcca7c25f35",
     "url": "/static/js/2.e8c325bf.chunk.js"
   },
   {
-    "revision": "bc0e02fd98a82d1c2c45",
-    "url": "/static/js/main.6f3d7dfd.chunk.js"
+    "revision": "4c4acf307b73ebea8d51",
+    "url": "/static/js/main.c00ee5e2.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.05a9f8185377efdcd047f43bc8eb3ab3.js"
+  "/precache-manifest.7c32bfb69cbdea70c62451bd2777762e.js"
 );
 
 self.addEventListener('message', (event) => {
