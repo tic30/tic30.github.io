7a2afbb9a8a5fbf502bacad344b70ffdc90052e2
Tim Chu <tchu@indeed.com>
2020-08-19 13:12:44 -0700

    Updates

---
+++ precache-manifest.7afc5e7d42bad2cec736fcc06b045e46.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d096b5164f1bb6e6befbafd68cec9c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1b30162fd49d03c964a",
+    "url": "/static/css/main.5827e3c0.chunk.css"
+  },
+  {
+    "revision": "a4707a9407f11644963c",
+    "url": "/static/js/2.be3403fd.chunk.js"
+  },
+  {
+    "revision": "a1b30162fd49d03c964a",
+    "url": "/static/js/main.af71cdaf.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5193cbedbc3eb60a2e671199ce11389b.js"
+  "/precache-manifest.7afc5e7d42bad2cec736fcc06b045e46.js"
 );
 
 self.addEventListener('message', (event) => {
