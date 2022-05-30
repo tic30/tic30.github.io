092b852bfab7d3b54fcf36c195ec93b9e02bba4b
Tim Chu <tc.timchu@gmail.com>
2022-05-30 15:22:11 -0700

    Initial commit

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,92 @@
+import { useState } from "react";
+import WorkspacesIcon from "@mui/icons-material/Workspaces";
+import PersonIcon from "@mui/icons-material/Person";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import DesignServicesIcon from "@mui/icons-material/DesignServices";
+import SummarizeIcon from "@mui/icons-material/Summarize";
+import BatchPredictionIcon from "@mui/icons-material/BatchPrediction";
+import CoffeeIcon from "@mui/icons-material/Coffee";
+import MemoryIcon from "@mui/icons-material/Memory";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import MenuScreen, { MenuItemType } from "../MenuScreen";
+import Texts from "../../texts";
+
+const menuList: MenuItemType[] = [
+  {
+    icon: <PersonIcon />,
+    text: "About Me",
+    link: "/home#self-intro",
+  },
+  {
+    icon: <DesignServicesIcon />,
+    text: "My work",
+    link: "/home#work",
+  },
+  {
+    icon: <BatchPredictionIcon />,
+    text: "Blog",
+    link: "/home#blog",
+  },
+  {
+    icon: <WorkspacesIcon />,
+    text: "Projects",
+    link: "/home#projects",
+    subMenuItems: [
+      {
+        ...Texts.OH,
+        icon: <MemoryIcon />,
+      },
+      {
+        ...Texts.ITS,
+        icon: <VisibilityIcon />,
+      },
+      {
+        title: "Others",
+        pageUrl: "/home#projects",
+        icon: <MoreHorizIcon />,
+      },
+    ],
+  },
+  {
+    icon: <CoffeeIcon />,
+    text: "Connect",
+    link: "/home#connect",
+    subMenuItems: [
+      {
+        icon: <SummarizeIcon />,
+        title: "Resume",
+        pageUrl:
+          "https://drive.google.com/open?id=0B1dSWHM51dn-RGJBNlJZNFdaNW8",
+        external: true,
+      },
+      {
+        icon: <LinkedInIcon />,
+        title: "LinkedIn",
+        pageUrl: "https://www.linkedin.com/in/tim-chu-980881a4",
+        external: true,
+      },
+      {
+        icon: <GitHubIcon />,
+        title: "GitHub",
+        pageUrl: "https://github.com/tic30",
+        external: true,
+      },
+    ],
+  },
+];
+
+const Header: React.FC = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    return (
+        <MenuScreen
+          open={menuOpen}
+          setOpen={() => setMenuOpen(open => !open)}
+          menuList={menuList}
+        />
+    );
+};
+
+export default Header;
---
+++ src/texts.ts
@@ -0,0 +1,184 @@
+const Text = {
+  Hero: {
+    title: "Hi, Welcome!",
+  },
+  SelfIntro: {
+    content:
+      "Embrace difference, seek innovative solutions, execute with broken-down practical tasks and improve myself along the way.",
+    icons: [
+      { src: "icons/reading.svg" },
+      { src: "icons/soccer.svg" },
+      { src: "icons/pingpong.svg" },
+      { src: "icons/archery.svg" },
+    ],
+  },
+  Company: {
+    content:
+      "Bridge design and engineering, iterate on ideas with front end prototyping and production code management.",
+    icons: [
+      { src: "icons/cmu.png", dataTarget: "Cmu" },
+      { src: "icons/deephow.png", dataTarget: "Deephow" },
+      { src: "icons/siemens.png", dataTarget: "Siemens" },
+      { src: "icons/indeed.png", dataTarget: "Indeed" },
+    ],
+  },
+  Indeed: {
+    titleIcon: "icons/indeed.png",
+    content:
+      "I am working as a UX Developer at Indeed. The motivation for me to go to work everyday is simply straightforward: to help people get jobs. The major chord of my work includes studying user, creating solutions and validating the effectiveness of the solutions through fast iterations of prototyping, testing, optimization and finally push to production.",
+  },
+  Deephow: {
+    titleIcon: "icons/deephow.png",
+    content:
+      "This AI start-up is incubated in Techstar by the hands of talents from Siemens UX team. I lead the front end architecture design and MVP development. During the short journey I established a highly-reusable and efficient development ecosystem, introduced a fully automated process from coding, testing, building, to deployment, and I advocated code ownership to boost productivity.",
+  },
+  Siemens: {
+    titleIcon: "icons/siemens.png",
+    content:
+      "The experience at Siemens is fast-paced. Engineers and designers sync daily to enable development and design to run in parallel. Thanks to the close engagement with stake holder, I am able to test creative sometimes crazy ideas, collect feedback and iterate quickly on not only functions and feature but also tools, platforms and service systems.",
+  },
+  Cmu: {
+    titleIcon: "icons/cmu.png",
+    content:
+      "I joined a group of HCI student to create AR application and study its social impact. I received my degree of Master of Science in Software Engineering at Carnegie Mellon University(CMU) in 2018. CMU courses paved solid ground in engineering which I benefit from util today and encouraged me to pursue a life-long learning.",
+  },
+  // portfolio
+  OH: {
+    title: "Micro Frontend Migration @ Indeed",
+    imgUrl: "indeed-oh.png",
+    subTitle:
+      "Design and migrate to Webpack 5 based micro frontend architecture.",
+    btnText: "Read more",
+    pageUrl: "/microfe",
+  },
+  ITS: {
+    title: "Intelligent Tutoring Systems",
+    imgUrl: "its.png",
+    subTitle: "Better teaching with AR.",
+    btnText: "Read more",
+    pageUrl: "/its",
+  },
+  DealFindMe: {
+    title: "DealFindMe",
+    imgUrl: "dealfindme.png",
+    subTitle: "Coupon at hand for the store I am standing in.",
+    btnText: "Coming Soon",
+    pageUrl: "", // "/dfm"
+  },
+  Milu: {
+    title: "Milu",
+    imgUrl: "milu.png",
+    subTitle: "An image content search engine.",
+    btnText: "Coming Soon",
+    pageUrl: "", // "/milu"
+  },
+  EthereumWallet: {
+    title: "Ethereum Wallet",
+    imgUrl: "ethereumwallet.png",
+    subTitle: "Checkout with Ethereum using this extension.",
+    btnText: "Coming Soon",
+    pageUrl: "", // "/ethereumwallet"
+  },
+  MovieEmodex: {
+    title: "Movie.Emodex",
+    imgUrl: "movie.png",
+    subTitle: "New movie rating based on emotional impact.",
+    btnText: "Coming Soon",
+    pageUrl: "", // "/movieemodex"
+  },
+  // ITS page
+  ITSHero: {
+    title: "INTELLIGENT TUTORING SYSTEMS",
+    content: "Research Project @ CMU.HCII",
+  },
+  ITSTech: [
+    {
+      src: "/imgs/icons/hololens.png",
+      msrc: "/imgs/icons/hololens_m.png",
+      alt: "Hololens",
+    },
+    {
+      src: "/imgs/icons/unity.png",
+      msrc: "/imgs/icons/unity_m.png",
+      alt: "Unity",
+    },
+    {
+      src: "/imgs/icons/vuforia.png",
+      msrc: "/imgs/icons/vuforia_m.png",
+      alt: "Vuforia",
+    },
+    {
+      src: "/imgs/icons/node.png",
+      msrc: "/imgs/icons/node.png",
+      alt: "NodeJS",
+    },
+    {
+      src: "/imgs/icons/mongo.png",
+      msrc: "/imgs/icons/mongo_m.png",
+      alt: "MongoDB",
+    },
+  ],
+  // Indeed page
+  IP: [
+    {
+      title: "UX Developer",
+      subtitle:
+        "My current job title is UX Developer. I play a role of front end contributor and design-engineering coordinator in the team. My work supports the front end feature iterations, drive adoption of company-wide UX & FE directions and guidelines, and coordinate across teams.",
+      contents: [
+        {
+          title: "For every employer",
+          content:
+            "In employer team, I focus on presenting easy to use flow and features for employers to post a job and manage postings. I build onboarding pages that boost initial engagement from small, medium businesses and enterprises; I craft story-telling features in employers' job dashboard to increase employers' awareness of Indeed features and help them quickly navigate to the right action to get the most hires.",
+          illustration: "/imgs/indeed-illstration1.png",
+        },
+        {
+          title: "One Experience",
+          content:
+            "Working in UX org, I push towards consistent user experience across Indeed products. I advocate the adoption of the latest design and UI libraries across teams, identify broken experiences and coordinate with engineers and other UX Developers to fix them. My believe is that the best way to build customer trust and brand recognition is through consistent visual presentation, high quality content, and productive features.",
+          illustration: "/imgs/indeed-illstration2.png",
+        },
+        {
+          title: "UI Library @ Indeed community",
+          content:
+            "I maintain one of the mostly adopted Indeed internal UI libraries. It is community based and I am there to encourage contribution and guide contributors. In addition, I work with the maintainer team to review code, refresh the repository and manage releases. We made sure each library has clear documentation, is generally reusable, has no feature overlap, and passes the highest standard of performance and accessibility check",
+          illustration: "/imgs/icons/gears.png",
+        },
+        {
+          title: "Engineering efficiency",
+          content:
+            "As a developer, I constantly look for ways to improve code quality and dev experience. I adopt patterns, technologies and introduce tools to help speed up the engineering process. I work with QA engineers to increase test coverage, create test plans, automate pipelines and document ways to track error and monitor performance.",
+          illustration: "/imgs/icons/arrows.png",
+        },
+        {
+          title: "Exploration, learning and growth",
+          content:
+            "3rd party open source libraries contribute to Indeed front end code base and I actively research on useful new gadgets and bring them to the company. Meanwhile, I organize ad hoc training and knowledge sharing sessions to onboard new members, learn from each other and share insights towards the future of front end.",
+          illustration: "/imgs/icons/high-five.png",
+        },
+      ],
+    },
+    {
+      title: "Design Technologist",
+      subtitle:
+        "In my early career, I was a Design Technologist(DT) where I supported the product lifecycle by iterating on user study, prototyping and AB testing.",
+      contents: [],
+      icons: [
+        "/imgs/icons/research.png",
+        "/imgs/icons/strategy.png",
+        "/imgs/icons/next-button.png",
+        "/imgs/icons/switch.png",
+      ],
+    },
+  ],
+  IndeedMicroFE: {
+    title:
+      "Migrate to Micro Frontend with Indeed Design System and Module Federation",
+    subtitle: "@ Indeed",
+    disclaimer:
+      "This is a personal narrative, not an Indeed publication, does not represent company voice and should not be quoted or republished for any reason.",
+    hero: "Upon the arrival of Webpack 5, we see an opportunity to significantly boost the page performance, enhance branding and user experience throughout the Indeed products, increase our system's robustness, and clearly define the ownership across teams. To achieve this goal, we refactored our company's entire front end infrastructure.",
+    p1: "Nobody likes heavy and slow websites. As Indeed grows and more features are added, performance of our pages decreases, UI inconsistency and broken experiences show up. UXers worked with engineers to apply patches to make improvements but at a certain point, we knew that starting from scratch would make more sense, especially when more powerful tools and concepts are introduced.",
+  },
+};
+
+export default Text;
