a54f0c04daaccf5d6d21d683f245e36039731e64
Tim Chu <tc.timchu@gmail.com>
2025-10-11 12:41:43 -0700

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    build: {
+        outDir: 'build',
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    vendor: [
+                        'react',
+                        'react-dom',
+                        'react-router-dom',
+                        'react-router-hash-link',
+                        '@mui/material',
+                        '@mui/icons-material',
+                        'motion/react',
+                    ],
+                },
+            },
+        },
+    },
+    plugins: [react()],
+});
